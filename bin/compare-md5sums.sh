7b31341654c33ca3b719dcaabc44b99b2e677928
aksoutherland <27647486+aksoutherland@users.noreply.github.com>
2025-01-30 09:20:54 -0600

    script to compare md5sums
    # this script will get the md5sum for each course file
    # then compare the new md5sum file with the file downloaded with the course
    # this script should be run from the directory you downloaded all files into

---
+++ bin/compare-md5sums.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this script will get the md5sum for each course file
+# then compare the new md5sum file with the file downloaded with the course
+# this script should be run from the directory you downloaded all files into 
+md5sum *.7z.??? > md5sums && diff md5sums *.7z.md5sums
+# 
