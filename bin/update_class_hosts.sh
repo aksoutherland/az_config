3a56232f4a6f7f89c25bba9afeb3de9971cd74e8
[aksoutherland] <[kd5mvd@gmail.com]>
2025-02-04 11:22:51 -0600

    fixed some syntx errors and typos for the usage function

--- bin/update_class_hosts.sh
+++ bin/update_class_hosts.sh
@@ -4,10 +4,6 @@
 # we use the command "az vm list-ip-addresses --output table | awk '{print $2,$1}' | egrep -v 'Public|----'#" to get a list of stations and add the station names to our local /etc/hosts file
 #
 
-if [ -z "$1" ]
-then
-	usage
-fi
 
 usage () {
         echo
@@ -15,9 +11,9 @@ usage () {
         echo
         echo "When running this script, you need to supply 1 argument"
         echo
-        echo "<MODE> will be either "add" to append the station names to your local /etc/hosts file"
+        echo "<MODE> will be either 'add' to append the station names to your local /etc/hosts file"
 	echo 
-	echo "or "remove" to delete the station names for your local /etc/hosts file"
+	echo "or 'remove' to delete the station names for your local /etc/hosts file"
 	echo
         echo "Please re-run the command with the proper argument"
         echo
@@ -25,15 +21,20 @@ usage () {
         echo
 }
 
+if [ -z "$1" ]
+then
+	usage
+fi
+
 case $1 in
 add) 
 # the first step is to back up your /etc/hosts file
 sudo cp /etc/hosts /etc/hosts-pre_class
-# now we need to get a list of stations from azure
+# now we need to get a list of stations from azure and append them to your existing /etc/hosts file
 az vm list-ip-addresses --output table | awk '{print $2,$1}' | egrep -v 'Public|----' | sudo tee -a /etc/hosts
 	;;
 remove)
-# now we restore /etc/hosts back to its original state
+# here we restore /etc/hosts back to its original state
 sudo cp /etc/hosts-pre_class /etc/hosts
 	;;
 *)
