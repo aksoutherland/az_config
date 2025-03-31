25f36501f6721f815c1f401abf0a290c16794412
[aksoutherland] <[kd5mvd@gmail.com]>
2025-03-31 14:03:05 -0500

    trying to add second user to guac

--- initdb.sql
+++ initdb.sql
@@ -808,3 +808,25 @@ SELECT * FROM guacamole_connection WHERE connection_name = 'rdp' AND parent_id I
 -- Add parameters to the new connection
 INSERT INTO guacamole_connection_parameter VALUES (2, 'hostname', 'demo3');
 INSERT INTO guacamole_connection_parameter VALUES (2, 'port', '3389');
+
+-- Create second user "tux" with password "guacadmin"
+INSERT INTO guacamole_entity (name, type) VALUES ('tux', 'USER');
+INSERT INTO guacamole_user (entity_id, password_hash, password_salt, password_date)
+SELECT
+    entity_id,
+    decode('CA458A7D494E3BE824F5E1E175A1556C0F8EEF2C2D7DF3633BEC4A29C4411960', 'hex'),  -- 'guacadmin'
+    decode('FE24ADC5E11E2B25288D1704ABE67A79E342ECC26064CE69C5B3177795A82264', 'hex'),
+    CURRENT_TIMESTAMP
+FROM guacamole_entity WHERE name = 'tux' AND guacamole_entity.type = 'USER';
+
+-- Grant admin permission to read/update/administer self
+INSERT INTO guacamole_user_permission (entity_id, affected_user_id, permission)
+SELECT guacamole_entity.entity_id, guacamole_user.user_id, permission::guacamole_object_permission_type
+FROM (
+    VALUES
+        ('tux', 'tux', 'READ'),
+) permissions (username, affected_username, permission)
+JOIN guacamole_entity          ON permissions.username = guacamole_entity.name AND guacamole_entity.type = 'USER'
+JOIN guacamole_entity affected ON permissions.affected_username = affected.name AND guacamole_entity.type = 'USER'
+JOIN guacamole_user            ON guacamole_user.entity_id = affected.entity_id;
+
