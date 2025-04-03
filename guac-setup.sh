c70204b1cb78a839dcba8aaae8ad19d55e9c5e0e
[aksoutherland] <[kd5mvd@gmail.com]>
2025-04-03 13:19:41 -0500

    addeded db init from git to the guac script

--- guac-setup.sh
+++ guac-setup.sh
@@ -2,7 +2,15 @@
 # This script will setup Guacamole on the host
 # Here we make sure that podman is installed
 # the folders being created for caddy are for future use
-sudo zypper in -y podman
+# here we are adding the ip/hostname to /etc/hosts
+# first we gather the details
+IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+NAME=$(hostname)
+# now we put those details into /etc/hosts
+echo $IP  $NAME | sudo tee -a /etc/hosts
+# now we install podman and xrdp if not installed
+sudo zypper in -y podman xrdp freerdp-server
+sudo systemctl enable --now xrdp
 # Now we create the required folders
 sudo mkdir -m 775 -p /podman/guac/home/.guacamole
 sudo mkdir -m 775 -p /podman/postgresql/{data,init}
@@ -11,12 +19,17 @@ sudo mkdir -m 775 -p /podman/caddy/{config,data}
 # Set the ownership on the folders
 sudo chown tux:users -R /podman
 # now we open the correct ports in the firewall
+sudo systemctl enable --now firewalld.service
 sudo firewall-cmd --add-port=8080/tcp --permanent
+sudo firewall-cmd --add-service=rdp --permanent
+sudo firewall-cmd --add-service=ssh --permanent
 sudo firewall-cmd --reload
 # # Create the Guacamole Network
 podman network create guacamole
 # Initialize the database for Guacamole
-podman run --rm docker.io/guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > /podman/postgresql/init/initdb.sql
+# podman run --rm docker.io/guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > /podman/postgresql/init/initdb.sql
+# this command can be used to grab a custom initdb.sql that can be used to initialze the DB for first time use
+wget https://github.com/aksoutherland/az_config/raw/master/initdb.sql -O /podman/postgresql/init/initdb.sql
 # Start the required containers starting with postgresql
 podman run -d --name postgresql \
 -v /podman/postgresql/init:/docker-entrypoint-initdb.d \
@@ -44,8 +57,7 @@ podman run -d --name guacamole \
 -e GUACD_PORT_4822_TCP_PORT=4822 \
 -e GUACD_HOSTNAME=guacd \
 -v /podman/guac/home:/etc/guacamole \
---requires=guacd \
---requires=postgresql \
+--requires=guacd,postgresql \
 -p 8080:8080 \
 --network=guacamole \
 docker.io/guacamole/guacamole
@@ -65,4 +77,3 @@ systemctl --user daemon-reload
 systemctl --user enable container-postgresql.service 
 systemctl --user enable container-guacd.service 
 systemctl --user enable container-guacamole.service 
-# need to add a section for the config for guac for user mappings and such
