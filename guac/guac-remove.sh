5ca012b231fe0170bc5e387d69047a200dbbaf13
[aksoutherland] <[kd5mvd@gmail.com]>
2025-04-16 11:51:10 -0500

    added comments to script so we know what is happening

--- guac/guac-remove.sh
+++ guac/guac-remove.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 # this script will remove podman for testing purposes
+echo "Removing Guacamole"
+# first we stop all containers
 podman stop -a
+# then we remove all of the containers
 podman rm -a
+# now we remove all of the artifacts
 podman system prune -af
+# here we remove the folder that contains the service files for the containers
 sudo rm -rf ~/.config/systemd/user
+# finally we remove the folders that podman was using
 sudo rm -rf /podman
+echo "Guacamole has been removed"
