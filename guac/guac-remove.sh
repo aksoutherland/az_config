e52834acb200b3a730d9d4a3ec60a2df60738b50
[aksoutherland] <[kd5mvd@gmail.com]>
2025-05-13 14:47:06 -0500

    added more commands to guac-remove

--- guac/guac-remove.sh
+++ guac/guac-remove.sh
@@ -7,8 +7,12 @@ podman stop -a
 podman rm -a
 # now we remove all of the artifacts
 podman system prune -af
-# here we remove the folder that contains the service files for the containers
+# here we clean up the folder that contains the service files for the containers
+systemctl --user disable container-postgresql.service > /dev/null 2>&1
+systemctl --user disable container-guacd.service > /dev/null 2>&1
+systemctl --user disable container-guacamole.service > /dev/null 2>&1
 sudo rm -rf ~/.config/systemd/user
+systemctl --user daemon-reload > /dev/null 2>&1
 # finally we remove the folders that podman was using
 sudo rm -rf /podman
 echo "Guacamole has been removed"
