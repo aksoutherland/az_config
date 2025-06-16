d61beab14c991127809280defbb83ac987605524
[aksoutherland] <[kd5mvd@gmail.com]>
2025-06-16 14:36:02 -0500

    removed files no longer needed

--- bin/tablet_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# to find the stylus ID - use the command
-# xsetwacom --list
-#
-# to find the monitor use the command
-# xrandr
-# use this command to map the stylus to a specific monitor
-xsetwacom set 15  MapToOutput 2560x1440+5120+0
-(/usr/bin/gromit-mpx >/dev/null 2>&1 &)
